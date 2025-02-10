5ef72d689caa8e1303749fc47e3e1ca462d75683
Girma35 <Girmawakeyo4@gmail.com>
2025-02-09 23:20:02 -1200

    Shaass and Oskols

--- Shaass and Oskols/solution.py
+++ Shaass and Oskols/solution.py
@@ -1,5 +1,22 @@
-import sys
+def update_birds_on_wires(n, birds, shots):
+    for x, y in shots:
+        x -= 1  
 
-sys.stdin = open('input.txt', 'r')
-sys.stdout = open('output.txt', 'w')
+        if x > 0:  
+            birds[x - 1] += y - 1  
+        if x < n - 1:  
+            birds[x + 1] += birds[x] - y  
 
+        birds[x] = 0  
+
+    return birds
+
+n = int(input())
+birds = list(map(int, input().split()))
+m = int(input())
+shots = [tuple(map(int, input().split())) for _ in range(m)]
+
+result = update_birds_on_wires(n, birds, shots)
+
+for count in result:
+    print(count)
